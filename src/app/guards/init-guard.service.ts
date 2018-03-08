ba423df994a6e02991a15db6d383ed0d192f669c
davidalexteixeira <david5teixeira@gmail.com>
2018-03-08 11:05:33 +0100

    modified init guard

--- src/app/guards/init-guard.service.ts
+++ src/app/guards/init-guard.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class InitGuardService {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): Promise<boolean> {
     return this.authService.me()
       .then((user) => {
-        return true;
+        this.router.navigate(['/playlist'])
+        return true
       })
       .catch((error) => {
         console.error(error);
