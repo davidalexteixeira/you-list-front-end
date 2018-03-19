d67b3e3c6e2d9acd6c5919033af4bc33c25b6960
davidalexteixeira <david5teixeira@gmail.com>
2018-03-19 16:11:30 +0100

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+declare var jquery: any;
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
